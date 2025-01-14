87a7bd04503d576fe4218c7fa7d76c5821200568
Matheus Pantoja de Morais <matheus.morais@PORTOVELHO.RO.GOV.BR>
2025-01-14 13:58:27 -0400

    Atualizações, modal adicionada a navbar, modificações pequenas.

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -8,6 +8,34 @@ document.addEventListener("DOMContentLoaded", function () {
         .then(data => {
             // Insere o conteúdo da navbar no elemento com id 'navbar-container'
             navbarContainer.innerHTML = data;
+
+            // Lógica para exibir a imagem selecionada ou mostrar o texto padrão
+            const inputFoto = document.getElementById('inputGroupFoto');
+            const previewContainer = document.getElementById('previewContainer');
+            const previewImage = document.getElementById('previewImage');
+            const noImageText = document.getElementById('noImageText');
+        
+            // Lógica para exibir a imagem selecionada ou mostrar o texto padrão
+            if (inputFoto) {
+                inputFoto.addEventListener('change', function(event) {
+                    const file = event.target.files[0];
+        
+                    if (file) {
+                        const reader = new FileReader();
+                        reader.onload = function(e) {
+                            // Exibe a imagem e esconde o texto
+                            previewImage.style.display = 'block';
+                            previewImage.src = e.target.result;
+                            noImageText.style.display = 'none';
+                        };
+                        reader.readAsDataURL(file);
+                    } else {
+                        // Se não houver imagem, mostra o texto
+                        previewImage.style.display = 'none';
+                        noImageText.style.display = 'block';
+                    }
+                });
+            }
         })
         .catch(error => console.error('Erro ao carregar a navbar:', error));
 });
