de36f84a6582d6cd6b36b02983eed700db3f4559
Matheus Pantoja de Morais <matheus.morais@PORTOVELHO.RO.GOV.BR>
2025-01-15 10:57:06 -0400

    Atualização nas modals da Navbar, criação de diferentes tipos de ficha

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Seleciona o container onde a navbar será inserida
     const navbarContainer = document.getElementById('navbar-container');
-    
+
     // Carrega o conteúdo do arquivo navbar.html
     fetch('/src/views/navbar.html')  // Caminho absoluto para a navbar.html
         .then(response => response.text())
@@ -14,15 +14,15 @@ document.addEventListener("DOMContentLoaded", function () {
             const previewContainer = document.getElementById('previewContainer');
             const previewImage = document.getElementById('previewImage');
             const noImageText = document.getElementById('noImageText');
-        
+
             // Lógica para exibir a imagem selecionada ou mostrar o texto padrão
             if (inputFoto) {
-                inputFoto.addEventListener('change', function(event) {
+                inputFoto.addEventListener('change', function (event) {
                     const file = event.target.files[0];
-        
+
                     if (file) {
                         const reader = new FileReader();
-                        reader.onload = function(e) {
+                        reader.onload = function (e) {
                             // Exibe a imagem e esconde o texto
                             previewImage.style.display = 'block';
                             previewImage.src = e.target.result;
